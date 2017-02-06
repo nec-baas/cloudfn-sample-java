354f95954bd105faf0bb39f592ef7d5d7f070c33
Kentaro Ozawa <k-ozawa@az.jp.nec.com>
2017-02-06 15:33:34 +0900

    サンプル向けの api, function 定義を追加。ハンドラの引数修正。

--- hello/src/main/java/com/nec/baas/cloudfn/sample/Hello.java
+++ hello/src/main/java/com/nec/baas/cloudfn/sample/Hello.java
@@ -1,12 +1,17 @@
 package com.nec.baas.cloudfn.sample;
 
 import com.nec.baas.cloudfn.sdk.Context;
+import java.util.Map;
 
 /**
  * Hello world!
  */
 public class Hello {
-    public static void hello(String name, Context context) {
-        context.succeed("Hello " + name + "!");
+    public static void sayHello(Map<String, Object> event, Context context) {
+        String name = "Jhon Doe";
+        if (context.clientContext().queryParams().containsKey("name")) {
+            name = context.clientContext().queryParams().get("name").get(0);
+        }
+        context.succeed("Hello, " + name + "!");
     }
 }
